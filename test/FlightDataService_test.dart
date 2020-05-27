bbb828eb7d52b80ce2f17882c4891d3ccc970ee4
Bennie van der Walt <bennie.vanderwalt@xnovos.com>
2020-05-27 09:36:23 +0200

    Icon change and typematcher change on unit test
    Changed TypeMatcher to IsA in the returns a flight if the http call completes successfully test

--- test/FlightDataService_test.dart
+++ test/FlightDataService_test.dart
@@ -1,13 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:spacex_flights/service_locator.dart';
 import 'package:spacex_flights/src/resources/repository.dart';
 import 'package:spacex_flights/src/models/flights.dart';
-import 'package:matcher/matcher.dart';
 import 'package:mockito/mockito.dart';
 import 'TestFlightObj.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   group('fetchFlightData', () {
     test('returns a flight if the http call completes successfully', () async {
       registerServices(testing: true);
@@ -18,7 +19,7 @@ void main() {
       when(client.get('https://api.spacexdata.com/v3/launches')).thenAnswer(
           (_) async => http.Response(TestFlightObj.flightJson, 200));
 
-      expect(await _repository.fetchAllFlights(), TypeMatcher<Flights>());
+      expect(await _repository.fetchAllFlights(), isA<Flights>());
     });
 
     test('throws an exception if the http call completes with an error', () {
