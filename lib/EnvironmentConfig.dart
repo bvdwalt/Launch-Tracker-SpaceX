2a4b1723569f7700ade78b1d806574753e721b66
Bennie van der Walt <barendjohannes.vdwalt@gmail.com>
2020-07-07 13:38:31 +0200

    Update EnvironmentConfig.dart
    EnvironmentConfig change for admob

--- lib/EnvironmentConfig.dart
+++ lib/EnvironmentConfig.dart
@@ -1,7 +1,12 @@
 class EnvironmentConfig {
   static const SENTRY_DSN = String.fromEnvironment('SENTRY_DSN');
-  static const Ad_Mob_App_ID = String.fromEnvironment('Ad_Mob_App_ID');
-  static const Ad_Mob_Banner_ID = String.fromEnvironment('Ad_Mob_Banner_ID');
+
+  static const Ad_Mob_App_ID = String.fromEnvironment('Ad_Mob_App_ID',
+      defaultValue: 'ca-app-pub-7724266756224086~6556146475');
+
+  static const Ad_Mob_Banner_ID = String.fromEnvironment('Ad_Mob_Banner_ID',
+      defaultValue: 'ca-app-pub-7724266756224086/4427201800');
+
   static const BASE_URL = String.fromEnvironment('BASE_URL',
       defaultValue: 'https://api.spacexdata.com/v3');
 }
